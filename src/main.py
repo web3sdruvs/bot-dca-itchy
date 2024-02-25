80d64ecf442807932214b5ecda7b758cfcb12aab
web3sdruvs.crypto <112975076+web3sdruvs@users.noreply.github.com>
2024-02-25 10:05:39 -0300

    docstring main

--- src/main.py
+++ src/main.py
@@ -86,6 +86,12 @@ re_default = r'[^a-zA-Z0-9\s]'
 
 #handler function is a function that deals with specific events or requests in a program
 def handler():
+    """
+    Handler function that processes events or requests for multiple tokens.
+
+    Returns:
+    - dict: A dictionary containing a simple status code.
+    """
     for i in range(len(token_symbol)): #enable this loop when it becomes possible to implement the purchase of more than 1 token
         qty_token = get_balance(token_symbol[i], current_time)
         price_low, price_current, price_last = get_price(token_symbol[i], current_time)
@@ -100,11 +106,33 @@ def handler():
  
 #get sign request secretkey to generate a signature
 def get_sign(api_secret, payload):
+    """
+    Generate a signature for authenticating an API request.
+
+    Parameters:
+    - api_secret (str): API secret key.
+    - payload (str): Request data to be signed.
+
+    Returns:
+    - str: Generated signature as a hexadecimal string.
+    """
     signature = hmac.new(api_secret.encode('utf-8'), payload.encode('utf-8'), digestmod=sha256).hexdigest()
     return signature
 
 #single request api call
 def send_request(method, path, urlpa, payload):
+    """
+    Make a call to the BingX API.
+
+    Parameters:
+    - method (str): HTTP method of the request (e.g., 'GET', 'POST').
+    - path (str): API endpoint path.
+    - urlpa (str): URL parameters.
+    - payload (str): Request data.
+
+    Returns:
+    - str: Content of the API response.
+    """
     url = '%s%s?%s&signature=%s' % (APIURL_BINGX, path, urlpa, get_sign(SECRETKEY_BINGX, urlpa))
     headers = {
         'X-BX-APIKEY': APIKEY_BINGX,
@@ -112,14 +140,32 @@ def send_request(method, path, urlpa, payload):
     response = requests.request(method, url, headers=headers, data=payload)
     return response.text
 
-#parameter encapsulation
 def praseParam(params_map):
+    """
+    Encapsulate parameters in a specific format for an API request.
+
+    Parameters:
+    - params_map (dict): Dictionary containing the request parameters.
+
+    Returns:
+    - str: Formatted string of parameters with timestamp added.
+    """
     sortedKeys = sorted(params_map)
     params_str = '&'.join(['%s=%s' % (x, params_map[x]) for x in sortedKeys])
     return params_str+'&timestamp='+str(int(time.time() * 1000))
 
-#get qty tokens balance
 def get_balance(symbol, timestamp):
+    """
+    Get the balance of a specific cryptocurrency symbol from the spot trading account.
+
+    Parameters:
+    - symbol (str): The cryptocurrency symbol (e.g., 'BTC', 'ETH').
+    - timestamp (str): Timestamp used for error logging.
+
+    Returns:
+    - float: The balance of the specified cryptocurrency symbol.
+    - str: If an error occurs, return the error message.
+    """
     payload = {}
     path = '/openApi/spot/v1/account/balance'
     method = 'GET'
@@ -142,8 +188,22 @@ def get_balance(symbol, timestamp):
                      +' \\(GMT\\-5\\)\\.\n\nFunction failure: get\\_balance\n\nError: '+error)
         return error
 
-#request withdraw
 def request_withdraw(symbol, network, address, addressTag, amount, timestamp):
+    """
+    Initiate a withdrawal request for a specific cryptocurrency.
+
+    Parameters:
+    - symbol (str): The cryptocurrency symbol (e.g., 'BTC', 'ETH').
+    - network (str): The network for the withdrawal (e.g., 'BTC', 'ERC20').
+    - address (str): The destination address for the withdrawal.
+    - addressTag (str): Address tag or memo for certain cryptocurrencies.
+    - amount (float): The amount of cryptocurrency to withdraw.
+    - timestamp (str): Timestamp used for error logging.
+
+    Returns:
+    - int: If successful, returns the withdrawal request ID.
+    - str: If an error occurs, returns the error message.
+    """
     payload = {}
     path = '/openApi/wallets/v1/capital/withdraw/apply'
     method = 'POST'
@@ -171,8 +231,19 @@ def request_withdraw(symbol, network, address, addressTag, amount, timestamp):
                      +' \\(GMT\\-5\\)\\.\n\nFunction failure: request\\_withdraw\n\nError: '+error)
         return error
     
-#fee withdraw
 def get_withdrawfee(symbol,network,timestamp):
+    """
+    Retrieve the withdrawal fee for a specific cryptocurrency and network.
+
+    Parameters:
+    - symbol (str): The cryptocurrency symbol (e.g., 'BTC', 'ETH').
+    - network (str): The network for the withdrawal (e.g., 'BTC', 'ERC20').
+    - timestamp (str): Timestamp used for error logging.
+
+    Returns:
+    - float: The withdrawal fee for the specified cryptocurrency and network.
+    - str: If an error occurs, returns the error message.
+    """
     network_fee = float(0.0)
     network = network.upper()
     payload = {}
@@ -199,8 +270,27 @@ def get_withdrawfee(symbol,network,timestamp):
                 +' \\(GMT\\-5\\)\\.\n\nFunction failure: get\\_withdrawfee\n\nError: '+error)
         return error
 
-#create an order
 def place_order(symbol, quantity, timestamp, index_current, index_class, dominance_btc_global, percent_1h_token, percent_24h_token,percent_7d_token, rsi_value, intensity):
+    """
+    Place a market buy order for a specified cryptocurrency symbol.
+
+    Parameters:
+    - symbol (str): The cryptocurrency symbol (e.g., 'BTC', 'ETH').
+    - quantity (float): The quantity of the cryptocurrency to buy.
+    - timestamp (str): Timestamp used for logging and notification.
+    - index_current (float): Number Fear and Greed Index.
+    - index_class (str): Classification Fear and Greed Index.
+    - dominance_btc_global (float): BTC dominance in the global market.
+    - percent_1h_token (float): 1-hour price change percentage for the token.
+    - percent_24h_token (float): 24-hour price change percentage for the token.
+    - percent_7d_token (float): 7-day price change percentage for the token.
+    - rsi_value (float): RSI (Relative Strength Index) value.
+    - intensity (float): Buy hand intensity value.
+
+    Returns:
+    - dict: If successful, returns a dictionary containing order details and market information.
+    - dict: If an error occurs, returns a dictionary with the 'statusCode' key set to 500.
+    """
     payload = {}
     path = '/openApi/spot/v1/trade/order'
     method = 'POST'
@@ -269,8 +359,19 @@ def place_order(symbol, quantity, timestamp, index_current, index_class, dominan
                      +' \\(GMT\\-5\\)\\.\n\nFunction failure: place\\_order\n\nError: '+error)
         return error
 
-#cancel an order
 def cancel_order(symbol, orderId, timestamp):
+    """
+    Cancel a specific order for a given cryptocurrency symbol.
+
+    Parameters:
+    - symbol (str): The cryptocurrency symbol (e.g., 'BTC', 'ETH').
+    - orderId (int): The ID of the order to be canceled.
+    - timestamp (str): Timestamp used for error logging and notification.
+
+    Returns:
+    - int: If successful, returns the client order ID of the canceled order.
+    - str: If an error occurs, returns the error message.
+    """
     payload = {}
     path = '/openApi/spot/v1/trade/cancel'
     method = 'POST'
@@ -292,8 +393,18 @@ def cancel_order(symbol, orderId, timestamp):
                      +' \\(GMT\\-5\\)\\.\n\nFunction failure: cancel\\_order\n\nError: '+error)
         return error
 
-#get prices current, open and low
 def get_price(symbol, timestamp):
+    """
+    Retrieve current price information for a given cryptocurrency symbol.
+
+    Parameters:
+    - symbol (str): The symbol of the cryptocurrency (e.g. 'BTC', 'ETH').
+    - timestamp (str): Timestamp used for recording and error notification.
+
+    Returns:
+    - tuple: If successful, returns a tuple containing the 24-hour minimum price, current price, and price changes.
+    - str: If an error occurs, returns the error message.
+    """
     payload = {}
     path = '/openApi/spot/v1/ticker/24hr'
     method = 'GET'
@@ -315,8 +426,18 @@ def get_price(symbol, timestamp):
                      +' \\(GMT\\-5\\)\\.\n\nFunction failure: price\\_current\n\nError: '+error)
         return error
 
-#get relative strength index 
 def get_rsi(symbol, timestamp):
+    """
+    Calculate the Relative Strength Index (RSI) for a given cryptocurrency symbol over a 14-day period.
+
+    Parameters:
+    - symbol (str): The cryptocurrency symbol (e.g., 'BTC', 'ETH').
+    - timestamp (str): Timestamp used for error logging and notification.
+
+    Returns:
+    - float: If successful, returns the calculated RSI value.
+    - str: If an error occurs, returns the error message.
+    """
     current_datetime = datetime.now()
     resultant_date = current_datetime - timedelta(days=14)
     desired_time = datetime(resultant_date.year, resultant_date.month, resultant_date.day, 15, 59, 59)
@@ -368,13 +489,29 @@ def get_rsi(symbol, timestamp):
                      +' \\(GMT\\-5\\)\\.\n\nFunction failure: get\\_rsi\n\nError: '+error)
         return error
 
-#send msg to telegram
 def bot_telegram(msg_text):
-  BOTKEY.sendMessage(CHATKEY, msg_text, parse_mode='MarkdownV2')
+    """
+    Send a message to a specified Telegram chat using the provided message text.
 
-#get the latest data of the fear and greed Index 
+    Parameters:
+    - msg_text (str): The text of the message to be sent.
+
+    Returns:
+    - None
+    """
+    BOTKEY.sendMessage(CHATKEY, msg_text, parse_mode='MarkdownV2')
+
 #index <= 25 : Extreme Fear | index <= 46 : Fear | index >=47 : Neutral | index >= 55 : Greed | index >= 76 : Extreme Greed
 def get_index_fear_greed(url):
+    """
+    Get the latest data of the Fear and Greed Index.
+
+    Parameters:
+    - url (str): The URL to retrieve the Fear and Greed Index data.
+
+    Returns:
+    - tuple: A tuple containing the index values for the current day, yesterday, last week, and last month.
+    """
     get_index_feargreed = requests.get(url)
     get_index_feargreed = get_index_feargreed.json()
     get_index_feargreed = get_index_feargreed
@@ -394,8 +531,16 @@ def get_index_fear_greed(url):
     index_last_month = int(sum(array_last_week)/array_len)
     return index_current, index_yesterday, index_last_week, index_last_month
 
-#get global market information at a glance
 def get_statistic_global(url):
+    """
+      Get crypto market insights.
+
+      Parameters:
+      - url (str): The URL to retrieve global market information.
+
+      Returns:
+      - tuple: a tuple containing the total market value, 24-hour trading volume and Bitcoin dominance.
+      """
     statistic_global = requests.get(url)
     statistic_global = statistic_global.json()
     total = statistic_global['total_market_cap_usd']
@@ -403,8 +548,17 @@ def get_statistic_global(url):
     dominance = statistic_global['bitcoin_percentage_of_market_cap']
     return total, volume, dominance
 
-#coin and token prices updated
 def get_statistic_token(symbol, url):
+    """
+    Get specific token statistics from a given URL based on the provided symbol.
+
+    Parameters:
+    - symbol (str): The symbol of the token (e.g., 'BTC', 'ETH').
+    - url (str): The URL to retrieve token statistics.
+
+    Returns:
+    - tuple: A tuple containing the 24-hour trading volume, market cap, and percentage changes in 1h, 24h, and 7d.
+    """
     statistic_token = requests.get(url)
     statistic_token =  statistic_token.json() 
     for i in statistic_token:
@@ -421,8 +575,22 @@ def get_statistic_token(symbol, url):
         volume = marketcap = percent_1h = percent_24h = percent_7d = 1e-7
         return volume, marketcap, percent_1h, percent_24h, percent_7d
         
-#withdraw balance and check if there are usdt funds in the wallet
 def check_balance_withdraw(balance, amount, symbol, network, address, tag, timestamp):  
+    """
+    Withdraw balance and check if there are USDT funds in the wallet.
+
+    Parameters:
+    - balance (float): The balance of the specified token.
+    - amount (float): The amount to withdraw.
+    - symbol (str): The symbol of the token (e.g., 'BTC', 'ETH').
+    - network (str): The network for withdrawal (e.g., 'ETH', 'TRX').
+    - address (str): The withdrawal address.
+    - tag (str): The withdrawal tag.
+    - timestamp (str): Timestamp used for error logging and notification.
+
+    Returns:
+    - None
+    """
     usdt = get_balance('USDT', timestamp)
     time.sleep(0.100)
     value = 5
@@ -447,6 +615,15 @@ def check_balance_withdraw(balance, amount, symbol, network, address, tag, times
         time.sleep(0.100)
     
 def indicators(symbol):
+    """
+    Calculate a set of indicators for a given cryptocurrency token.
+
+    Parameters:
+    - symbol (str): The symbol of the token (e.g., 'BTC', 'ETH').
+
+    Returns:
+    - tuple: A tuple containing the calculated indicators for the token.
+    """
     index_current, index_yesterday, index_last_week, index_last_month = get_index_fear_greed(get_index_feargreed)
     total_global, volume_global, dominance_btc_global = get_statistic_global(statistic_global)
     volume_24h_token, total_marketcap_token, percent_1h_token, percent_24h_token, percent_7d_token = get_statistic_token(symbol if 'WBTC' != symbol else 'BTC', statistic_token)
@@ -542,12 +719,24 @@ def indicators(symbol):
 
 #buy using dca by intensity
 def buy_dca(symbol, quantity, usdt,timestamp):
-  index_current, index_class, dominance_btc_global, percent_1h_token, percent_24h_token,percent_7d_token, rsi_value, total_end = indicators(symbol)
-  quantity = quantity*total_end
-  quantity = 5 if quantity < 5 else quantity
-  quantity = usdt if quantity > usdt else quantity
-  orderid, priceorder, qty, status = place_order(symbol, quantity, timestamp, index_current, index_class, dominance_btc_global, percent_1h_token, percent_24h_token,percent_7d_token, rsi_value, total_end)
-  return orderid, priceorder, qty, status
+    """
+    Buy using Dollar-Cost Averaging (DCA) strategy by intensity.
+
+    Parameters:
+    - symbol (str): The symbol of the token (e.g., 'BTC', 'ETH').
+    - quantity (float): The initial quantity to buy based on intensity.
+    - usdt (float): The available USDT balance for the purchase.
+    - timestamp (str): Timestamp used for order placement and logging.
+
+    Returns:
+    - tuple: A tuple containing the order ID, price, quantity, and status of the DCA purchase.
+    """
+    index_current, index_class, dominance_btc_global, percent_1h_token, percent_24h_token,percent_7d_token, rsi_value, total_end = indicators(symbol)
+    quantity = quantity*total_end
+    quantity = 5 if quantity < 5 else quantity
+    quantity = usdt if quantity > usdt else quantity
+    orderid, priceorder, qty, status = place_order(symbol, quantity, timestamp, index_current, index_class, dominance_btc_global, percent_1h_token, percent_24h_token,percent_7d_token, rsi_value, total_end)
+    return orderid, priceorder, qty, status
 
 #execute
 handler()
