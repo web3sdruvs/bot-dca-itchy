176a95c9c59efcc4e54f05136a6d673b65a9d69c
web3sdruvs.crypto <112975076+web3sdruvs@users.noreply.github.com>
2024-04-07 00:34:49 -0300

    bug fix untraded tokens

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -612,12 +612,12 @@ def buy_dca(symbol, quantity, usdt,timestamp):
     quantity = 5 if quantity < 5 else quantity
     quantity = usdt if quantity > usdt else quantity
         
-    if total_end > 0.70:
+    if total_end > 0.51:
         orderid, priceorder, qty, status = place_order(symbol, quantity, timestamp, index_current, index_class, dominance_btc_global, percent_1h_token, percent_24h_token,percent_7d_token, rsi_value, total_end)
         return orderid, priceorder, qty, status
     else:
         bot_telegram('⚠️Alert\\!\n\nPurchase not executed, price too inflated\\.\n\n'
-                +'Index: '+str(index_current).replace('.', '\\.')+' \\('+index_class+'\\)'
+                +'Token: '+symbol+'\nIndex: '+str(index_current).replace('.', '\\.')+' \\('+index_class+'\\)'
                 +'\nIntensity: '+str(round(total_end,2)).replace('.', '\\.').replace('-', '\\-')
                 +'\nRSI: '+str(round(rsi_value,2)).replace('.', '\\.')+'%'
                 +'\nBTC Dominance: '+str(round(dominance_btc_global,2)).replace('.', '\\.')+'%'
