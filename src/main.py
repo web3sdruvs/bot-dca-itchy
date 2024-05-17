af1022e69dd4898d36dbf6646fcd96b43a1db911
web3sdruvs.crypto <112975076+web3sdruvs@users.noreply.github.com>
2024-05-17 14:28:53 -0300

    logging

--- src/main.py
+++ src/main.py
@@ -32,8 +32,8 @@ def lambda_handler(event, context):
         qty_token = get_balance(token_symbol[i], current_time)
         price_low, price_current, price_last = get_price(token_symbol[i], current_time)
         if qty_token is not None:
+            info(f'Function lambda handler: token {token_symbol[i]}')
             check_balance_and_trade(round(qty_token * price_current,2), qty_token, token_symbol[i], network_blockchain[i], address_destination[i], address_tag[i], current_time)
-            info(f'Function lambda handler: token {token_symbol}')
         else: 
             bot_telegram('❌Alert\\!\n\nAPI error on '+current_time.replace('-', '\\-')
                 +' \\(GMT\\-5\\)\\.\n\nFunction failure: qty\\_token is null')
