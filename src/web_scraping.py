fd4bef41666b0920bdc180126203d2c44ade41ff
web3sdruvs.crypto <112975076+web3sdruvs@users.noreply.github.com>
2024-08-31 21:56:49 -0300

    create ws IndexFearGreed

--- src/web_scraping.py
+++ src/web_scraping.py
@@ -1,102 +1,65 @@
-from logging import info, error, basicConfig, INFO
-from web_scraping import IndexFearGreed
-import transaction_handler
+from urllib3.exceptions import InsecureRequestWarning
+from bs4 import BeautifulSoup
+from time import sleep
+import warnings
 import requests
 
-basicConfig(level=INFO, format='%(asctime)s - %(levelname)s - %(message)s')   
-    
-#index <= 25 : Extreme Fear | index <= 46 : Fear | index >=47 : Neutral | index >= 55 : Greed | index >= 76 : Extreme Greed
-def get_index_fear_greed(url):
-    """
-    Get the latest data of the Fear and Greed Index.
+warnings.simplefilter('ignore', InsecureRequestWarning)
+
+class IndexFearGreed:
+  def __init__(self, find='a', url='https://coinmarketcap.com'):
+    '''
+    Constructs all the necessary attributes for the IndexFearGreed object.
 
     Parameters:
-    - url (str): The URL to retrieve the Fear and Greed Index data.
+        find (str): The HTML element to find in the webpage (default is 'a').
+        url (str): The URL of the webpage to scrape (default is 'https://coinmarketcap.com').
+    '''
+    self.find = find.lower()
+    self.url = url.lower()
+
+  def __get_web_scraping(self):
+    '''
+    Perform web scraping on the given URL and return a list of found elements.
+
+    This method sends a GET request to the specified URL with appropriate headers,
+    parses the HTML content using BeautifulSoup, and finds all elements matching
+    the specified tag.
 
     Returns:
-    - tuple: A tuple containing the index values for the current day, yesterday, last week, and last month.
-    """
-    info(f'Function get_index_fear_greed')
-    get_index_feargreed = requests.get(url)
-    get_index_feargreed = get_index_feargreed.json()
-    get_index_feargreed = get_index_feargreed
-    array = []
+        list: A list of found HTML elements.
+    '''
+    _headers = {
+          'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:49.0) Gecko/20100101 Firefox/49.0',
+          'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+          'Accept-Language': 'pt-BR,pt;q=0.5',
+          'Accept-Encoding': 'gzip, deflate',
+          'Upgrade-Insecure-Requests': '1'
+    }
 
-    for item in get_index_feargreed['data']:
-        value = int(item['value'])
-        array.append(value)
+    with requests.Session() as session:
+      _response = session.get(f'{self.url}', headers = _headers, verify = True)
+      _response.raise_for_status()
+      _bs4_html = BeautifulSoup(_response.content, "html.parser")
+      _element = _bs4_html.find_all(self.find)
+    return _element
 
-    index_current = int(get_index_feargreed['data'][0]['value'])
-    index_yesterday = int(get_index_feargreed['data'][1]['value'])
-    array_last_month = array[:30]        
-    array_len = len(array_last_month)
-    index_last_week = int(sum(array_last_month)/array_len)
-    array_last_week = array[:7]
-    array_len = len(array_last_week)
-    index_last_month = int(sum(array_last_week)/array_len)
-    index_coinmarketcap = int(IndexFearGreed(find='span').get_index_fear_greed_coinmarketcap())
-    
-    if index_coinmarketcap != -1:
-        index_current = int((index_coinmarketcap + index_current) / 2)
-    
-    return index_current, index_yesterday, index_last_week, index_last_month
+  def get_index_fear_greed_coinmarketcap(self):
+    '''
+    Extract the Fear & Greed index from the scraped elements and return it as an integer.
 
-def get_statistic_global(url):
-    """
-    Get crypto market insights.
-
-    Parameters:
-    - url (str): The URL to retrieve global market information.
+    This method calls the __get_web_scraping method to retrieve the HTML elements,
+    extracts the Fear & Greed index from the specified element, and returns it as an integer.
 
     Returns:
-    - tuple: a tuple containing the total market value, 24-hour trading volume and Bitcoin dominance.
-    """
-    info(f'Function get_statistic_global')
-    statistic_global = requests.get(url)
-    statistic_global = statistic_global.json()
-    total = statistic_global['total_market_cap_usd']
-    volume = statistic_global['total_24h_volume_usd']
-    dominance = statistic_global['bitcoin_percentage_of_market_cap']
-    return total, volume, dominance
-
-def get_statistic_token(symbol, url, timestamp):
-    """
-    Get specific token statistics from a given URL based on the provided symbol.
-
-    Parameters:
-    - symbol (str): The symbol of the token (e.g., 'BTC', 'ETH').
-    - url (str): The URL to retrieve token statistics.
-
-    Returns:
-    - tuple: A tuple containing the 24-hour trading volume, market cap, and percentage changes in 1h, 24h, and 7d.
-    """
-    info(f'Function get_statistic_token')
-    statistic_token = requests.get(url)
-    statistic_token =  statistic_token.json() 
-    for i in statistic_token:
-        i['id'] = i['symbol']   
+        int: The Fear & Greed index if successful, or -1 if an error occurs or not enough elements are found.
+    '''
     try:
-        statistic_token = [i for i in statistic_token if i['id'] == symbol]
-        volume = float(statistic_token[0]['24h_volume_usd'])
-        marketcap = float(statistic_token[0]['market_cap_usd'])
-        percent_1h = float(statistic_token[0]['percent_change_1h'])
-        percent_24h = float(statistic_token[0]['percent_change_24h'])
-        percent_7d = float(statistic_token[0]['percent_change_7d'])
-        return volume, marketcap, percent_1h, percent_24h, percent_7d    
-    except:
-        try:
-            #Abstract into a later function
-            candlestick_list = transaction_handler.get_candlestick_chart_data('XRD', timestamp, 2, '1h')
-            percent_1h = ((candlestick_list['data'][0][4]/candlestick_list['data'][-1][4])-1)*100
-
-            candlestick_list = transaction_handler.get_candlestick_chart_data('XRD', timestamp, 2, '1d')
-            percent_24h = ((candlestick_list['data'][0][4]/candlestick_list['data'][-1][4])-1)*100
-            volume = candlestick_list['data'][0][5]
-            marketcap = (candlestick_list['data'][0][5])*20
-            
-            candlestick_list = transaction_handler.get_candlestick_chart_data('XRD', timestamp, 7, '1d')
-            percent_7d = ((candlestick_list['data'][0][4]/candlestick_list['data'][-1][4])-1)*100
-            return volume, marketcap, percent_1h, percent_24h, percent_7d
-        except:
-            volume = marketcap = percent_1h = percent_24h = percent_7d = 1e-7
-            return volume, marketcap, percent_1h, percent_24h, percent_7d
+      _element = self.__get_web_scraping()
+      if len(_element) > 10:
+        _index = int(_element[10].text.split('/')[0])
+        return _index
+      else:
+        return -1
+    except Exception as e:
+      return -1
